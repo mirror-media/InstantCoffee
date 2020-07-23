830898ee06feb8fe2455438502409c5157726619
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2020-07-23 17:23:50 +0800

    Fix NewsMarqueeWidget.

--- lib/widgets/newsMarqueeWidget.dart
+++ lib/widgets/newsMarqueeWidget.dart
@@ -2,6 +2,7 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:readr_app/models/recordList.dart';
 import 'package:readr_app/storyPage.dart';
+import 'package:readr_app/widgets/marqueeWidget.dart';
 
 class NewsMarqueeWidget extends StatefulWidget {
   final RecordList recordList;
@@ -21,16 +22,11 @@ class NewsMarqueeWidget extends StatefulWidget {
 }
 
 class _MarqueeWidgetState extends State<NewsMarqueeWidget> {
-  int _index;
   CarouselOptions _options;
   CarouselController _carouselController = CarouselController();
-  List<ScrollController> _scrollController = List<ScrollController>();
 
   @override
   void initState() {
-    _index = 0;
-    widget.recordList.forEach((element) { _scrollController.add(ScrollController()); });
-
     _options = CarouselOptions(
       scrollPhysics: NeverScrollableScrollPhysics(),
       height: 32,
@@ -41,41 +37,23 @@ class _MarqueeWidgetState extends State<NewsMarqueeWidget> {
       autoPlayInterval: Duration(milliseconds: 5000),
       enableInfiniteScroll: false,
       enlargeCenterPage: false,
-      onPageChanged: (index, reason) {
-        setState(() {
-          _index = index;
-        });
-      },
+      onPageChanged: (index, reason) {},
     );
     super.initState();
   }
 
-  void _scroll() async {
-    await Future.delayed(widget.pauseDuration);
-
-    if (_scrollController[_index].hasClients) {
-      await _scrollController[_index].animateTo(
-          _scrollController[_index].position.maxScrollExtent,
-          duration: widget.animationDuration,
-          curve: Curves.easeIn);
-    }
-
-    await Future.delayed(widget.pauseDuration);
-  }
-
   @override
   Widget build(BuildContext context) {
-    _scroll();
     var width = MediaQuery.of(context).size.width;
 
     return CarouselSlider(
-      items: _buildList(width, widget.recordList, _scrollController[_index]),
+      items: _buildList(width, widget.recordList),
       carouselController: _carouselController,
       options: _options,
     );
   }
 
-  List<Widget> _buildList(double width, RecordList recordList, ScrollController controller) {
+  List<Widget> _buildList(double width, RecordList recordList) {
     List<Widget> resultList = List<Widget>();
     for(int i=0; i<recordList.length; i++)
     {
@@ -83,11 +61,9 @@ class _MarqueeWidgetState extends State<NewsMarqueeWidget> {
         InkWell(
           child: SizedBox(
             width: width,
-            child: SingleChildScrollView(
-              physics: NeverScrollableScrollPhysics(),
+            child: MarqueeWidget(
               child: Text(recordList[i].title, style: TextStyle(fontSize: 20),),
-              scrollDirection: widget.direction,
-              controller: controller,
+              animationDuration: Duration(milliseconds: 4000),
             ),
           ),
           onTap: () {
