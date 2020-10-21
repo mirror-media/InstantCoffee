51bd7b424ba4357416b1341fd41dbe37cf5edcaa
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2020-10-21 15:18:54 +0800

    Update banner ad displaying method.

--- lib/widgets/mMAdBanner.dart
+++ lib/widgets/mMAdBanner.dart
@@ -20,7 +20,6 @@ class MMAdBanner extends StatefulWidget {
 }
 
 class _MMAdBannerState extends State<MMAdBanner> with AutomaticKeepAliveClientMixin {
-  bool _visible = false;
   double _widthPadding = 0;
   double _padding = 0;
 
@@ -37,14 +36,26 @@ class _MMAdBannerState extends State<MMAdBanner> with AutomaticKeepAliveClientMi
     super.initState();
   }
 
+  Future<bool> _initAd() async{
+    if(Platform.isAndroid) {
+      // Prevent the lag of displaying the ad on android.
+      await Future.delayed(Duration(milliseconds: 300));
+      return true;
+    } else if(Platform.isIOS) {
+      // Run this before displaying any ad on iOS.
+      return await Admob.requestTrackingAuthorization();
+    }
+
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
     var width = MediaQuery.of(context).size.width - 2*_widthPadding;
 
     return FutureBuilder<bool>(
-      // Run this before displaying any ad.
-      future: Admob.requestTrackingAuthorization(),
+      future: _initAd(),
       builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
         if (snapshot.hasData && snapshot.data) {
           return Padding(
@@ -52,31 +63,71 @@ class _MMAdBannerState extends State<MMAdBanner> with AutomaticKeepAliveClientMi
             child: Container(
               width: width,
               height: width*widget.adSize.height/widget.adSize.width,
-              child: AnimatedOpacity(
-                opacity: _visible ? 1.0 : 0.0,
-                duration: Duration(milliseconds: 500),
-                child: AdmobBanner(
-                  adUnitId: widget.adUnitId,
-                  adSize: widget.adSize,
-                  listener: (AdmobAdEvent event, Map<String, dynamic> args) async{
-                    print(event.toString());
-                    if(event == AdmobAdEvent.loaded && !_visible && Platform.isAndroid) {
-                      //await Future.delayed(Duration(milliseconds: 500));
-                      if(mounted) {
-                        setState(() {
-                          _visible = true;
-                        });
-                      }
-                    }
-                  },
-                ),
+              color: Colors.transparent,
+              child: AnimatedOpacityBanner(
+                adUnitId: widget.adUnitId,
+                adSize: widget.adSize, 
               ),
             ),
           );
         }
-
+        
+        if(Platform.isAndroid) {
+          return Container(
+            width: width,
+            height: width*widget.adSize.height/widget.adSize.width,
+            color: Colors.transparent,
+          );
+        }
         return Container();
       }
+    );
+  }
+}
+
+class AnimatedOpacityBanner extends StatefulWidget {
+  final String adUnitId;
+  final AdmobBannerSize adSize;
+  AnimatedOpacityBanner({
+    @required this.adUnitId,
+    @required this.adSize,
+  });
+
+  @override
+  _AnimatedOpacityBannerState createState() => _AnimatedOpacityBannerState();
+}
+
+class _AnimatedOpacityBannerState extends State<AnimatedOpacityBanner> {
+  bool _visible = false;
+
+  @override
+  void initState() {
+    if(Platform.isIOS) {
+      _visible = true;
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedOpacity(
+      opacity: _visible ? 1.0 : 0.0,
+      duration: Duration(milliseconds: 300),
+      child: AdmobBanner(
+        adUnitId: widget.adUnitId,
+        adSize: widget.adSize,
+        listener: (AdmobAdEvent event, Map<String, dynamic> args) async{
+          print(event.toString());
+          if(event == AdmobAdEvent.loaded && !_visible && Platform.isAndroid) {
+            //await Future.delayed(Duration(milliseconds: 500));
+            if(mounted) {
+              setState(() {
+                _visible = true;
+              });
+            }
+          }
+        },
+      ),
     );
   }
 }
