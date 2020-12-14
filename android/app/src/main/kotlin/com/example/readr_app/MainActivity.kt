a9b2ebfd10beeb59bef97510df3fc1b328de3c48
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2020-12-14 18:14:19 +0800

    Fix appsflyer and fcm conflict bug.

--- android/app/src/main/kotlin/com/example/readr_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/readr_app/MainActivity.kt
@@ -1,21 +1,18 @@
 package com.example.readr_app
 
-import android.content.Intent;
-import android.os.Bundle;
-import androidx.annotation.NonNull;
+import android.content.Intent
+import android.os.Bundle
+import androidx.annotation.NonNull
 
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
-    // TODO: need to fix
-    // This can fix an appsflyer feature.
-    // Trigger the event from appOpenAttributionStream after 
-    // opening the deep link when app is working in the background.
-    // But it will cause another problem that 
-    // firebase cloud message can not trigger the onResume function 
-    // when app is working in the background.
-    // So I comment this temporarily.
-    // override fun onNewIntent(@NonNull intent : Intent){
-    //     setIntent(intent)
-    // }
+    override fun onNewIntent(@NonNull intent : Intent){
+        var uri: String? = intent.data?.toString()
+        if (uri != null && uri!!.startsWith("https://mirrormedia.onelink.me")) {
+            setIntent(intent)
+        } else {
+            super.onNewIntent(intent)
+        }
+    }
 }
