718510798ad62422235793d00728a948294b0cdb
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-01-27 17:37:04 +0800

    Add is not member story page cache.

--- lib/services/storyService.dart
+++ lib/services/storyService.dart
@@ -1,13 +1,22 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
 import 'package:readr_app/env.dart';
 import 'package:readr_app/helpers/apiBaseHelper.dart';
+import 'package:readr_app/helpers/appException.dart';
+import 'package:readr_app/helpers/cacheDurationCache.dart';
+import 'package:readr_app/helpers/mMCacheManager.dart';
 import 'package:readr_app/models/storyRes.dart';
 
 class StoryService {
+  MMCacheManager _mMCacheManager;
   ApiBaseHelper _helper;
   FirebaseAuth _auth;
   
   StoryService() {
+    _mMCacheManager = MMCacheManager();
     _helper = ApiBaseHelper();
     _auth = FirebaseAuth.instance;
   }
@@ -21,14 +30,41 @@ class StoryService {
         '}&related=full';
     //&clean=content
 
-    final jsonResponse = await _helper.getByUrl(
-      endpoint,
-      headers: {
-        'Cache-control': 'no-cache',
-        "Authorization": "Bearer $token"
-      },
-    );
+    bool isCacheFileExists = await _mMCacheManager.isFileExistsAndNotExpired(endpoint);
+    var jsonResponse;
+    Uint8List fileBytes;
+    if(isCacheFileExists) {
+      jsonResponse = await _helper.getByCache(endpoint);
+    } else {
+      try {
+        final response = await http.get(
+          endpoint, 
+          headers: {
+            'Cache-control': 'no-cache',
+            "Authorization": "Bearer $token"
+          },
+        );
+        fileBytes = response.bodyBytes;
+        jsonResponse = returnResponse(response);
+      } on SocketException {
+        print('No Internet connection');
+        throw FetchDataException('No Internet connection');
+      } catch(e) {
+        print('error: $e');
+      }
+      print('Api get done.');
+    }
+
     StoryRes storyRes = StoryRes.fromJson(jsonResponse);
+    if(!isCacheFileExists && !storyRes.story.categories.isMemberOnly()) {
+      // save cache file
+      _mMCacheManager.putFile(
+        endpoint, 
+        fileBytes, 
+        maxAge: storyCacheDuration, 
+        fileExtension: 'json'
+      );
+    }
     return storyRes;
   }
 }
