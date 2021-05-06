19483f4e2811434625d1d43ea5145076ed6035cb
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-05-06 18:32:25 +0800

    add create user with email and password api

--- lib/services/emailSignInService.dart
+++ lib/services/emailSignInService.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:readr_app/models/firebaseLoginStatus.dart';
 
 abstract class EmailSignInRepos {
   Future<List<String>> fetchSignInMethodsForEmail(String email);
+  Future<FirebaseLoginStatus> createUserWithEmailAndPassword(String email, String password);
 }
 
 class EmailSignInServices implements EmailSignInRepos{
@@ -15,5 +17,23 @@ class EmailSignInServices implements EmailSignInRepos{
       print('Fetch sign in methods for email: $onError');
     }
     return resultList;
+  }
+
+  Future<FirebaseLoginStatus> createUserWithEmailAndPassword(String email, String password) async {
+    try {
+      // Once signed in, get the UserCredential
+      await _auth.createUserWithEmailAndPassword(email: email, password: password);
+    } catch(onError) {
+      print('Error create user with email and password: $onError');
+      return FirebaseLoginStatus(
+        status: FirebaseStatus.Error,
+        message: onError.toString(),
+      );
+    }
+
+    return FirebaseLoginStatus(
+      status: FirebaseStatus.Success,
+      message: 'Create user with email and password: with firebase success',
+    );
   }
 }
