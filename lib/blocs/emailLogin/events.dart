74324cceb003ca4cca350f4a3e24ada7c79097a7
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-05-07 12:10:41 +0800

    add email login bloc

---
+++ lib/blocs/emailLogin/events.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+import 'package:readr_app/blocs/emailLogin/states.dart';
+import 'package:readr_app/helpers/exceptions.dart';
+import 'package:readr_app/models/firebaseLoginStatus.dart';
+import 'package:readr_app/services/emailSignInService.dart';
+
+
+abstract class EmailLoginEvents{
+  Stream<EmailLoginState> run(EmailSignInRepos emailSignInRepos);
+}
+
+class SignInWithEmailAndPassword extends EmailLoginEvents {
+  final String email;
+  final String password;
+  SignInWithEmailAndPassword({
+    this.email, 
+    this.password,
+  });
+
+  @override
+  String toString() => 'SignInWithEmailAndPassword { email: $email, password: ****** }';
+
+  @override
+  Stream<EmailLoginState> run(EmailSignInRepos emailSignInRepos) async*{
+    print(this.toString());
+    try{
+      yield EmailLoginLoading();
+      FirebaseLoginStatus frebaseLoginStatus = await emailSignInRepos.signInWithEmailAndPassword(email, password);
+      if(frebaseLoginStatus.status == FirebaseStatus.Success) {
+        yield EmailLoginSuccess();
+      } else if(frebaseLoginStatus.status == FirebaseStatus.Error) {
+        if(frebaseLoginStatus.message == '[firebase_auth/wrong-password] The password is invalid or the user does not have a password.') {
+          yield EmailLoginFail();
+        } else {
+          yield EmailLoginError(
+            error: UnknownException(frebaseLoginStatus.message),
+          );
+        }
+      } 
+    } on SocketException {
+      yield EmailLoginError(
+        error: NoInternetException('No Internet'),
+      );
+    } on HttpException {
+      yield EmailLoginError(
+        error: NoServiceFoundException('No Service Found'),
+      );
+    } on FormatException {
+      yield EmailLoginError(
+        error: InvalidFormatException('Invalid Response format'),
+      );
+    } catch (e) {
+      yield EmailLoginError(
+        error: UnknownException(e.toString()),
+      );
+    }
+  }
+}
---
+++ lib/blocs/emailLogin/states.dart
@@ -0,0 +1,14 @@
+abstract class EmailLoginState {}
+
+class EmailLoginInitState extends EmailLoginState {}
+
+class EmailLoginLoading extends EmailLoginState {}
+
+class EmailLoginSuccess extends EmailLoginState {}
+
+class EmailLoginFail extends EmailLoginState {}
+
+class EmailLoginError extends EmailLoginState {
+  final error;
+  EmailLoginError({this.error});
+}
