82503727992dd35747b1b6c9aa33bb42e258dc00
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-05-18 15:56:26 +0800

    add new magazine page and UI

---
+++ lib/pages/magazine/magazinePage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:readr_app/blocs/magazine/bloc.dart';
+import 'package:readr_app/helpers/dataConstants.dart';
+import 'package:readr_app/pages/magazine/magazineWidget.dart';
+import 'package:readr_app/services/magazineService.dart';
+
+class MagazinePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildBar(context),
+      body:  ListView(
+        children: [
+          BlocProvider(
+            create: (context) => MagazineBloc(magazineRepos: MagazineServices()),
+            child: MagazineWidget(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: Icon(Icons.arrow_back_ios),
+        onPressed: () => Navigator.of(context).pop(),
+      ),
+      centerTitle: true,
+      title: Text('下載電子雜誌'),
+      backgroundColor: appColor,
+    );
+  }
+}
