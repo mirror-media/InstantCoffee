2403e6aebcb252ad05c660ca5d7d9cf6daef8827
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-05-19 15:49:00 +0800

    add fetch next magazine list page by type api in magazine services

--- lib/services/magazineService.dart
+++ lib/services/magazineService.dart
@@ -6,17 +6,26 @@ import 'package:readr_app/models/magazineList.dart';
 abstract class MagazineRepos {
   /// type: special or weekly
   Future<MagazineList> fetchMagazineListByType(String type, {int page = 1, int maxResults = 8});
+  Future<MagazineList> fetchNextMagazineListPageByType(String type, {int maxResults = 8});
 }
 
 class MagazineServices implements MagazineRepos{
   ApiBaseHelper _helper = ApiBaseHelper();
+  int _page = 1;
 
   Future<MagazineList> fetchMagazineListByType(String type, {int page = 1, int maxResults = 8}) async {
     final jsonResponse = await _helper.getByCacheAndAutoCache(
-      env.baseConfig.magazinesApi + '?max_results=$maxResults&sort=-publishedDate&page&where={"type":{"\$in":["$type"]}}', 
+      env.baseConfig.magazinesApi + '?max_results=$maxResults&sort=-publishedDate&page=$page&where={"type":{"\$in":["$type"]}}', 
       maxAge: magazineCacheDuration
     );
     MagazineList magazineList = MagazineList.fromJson(jsonResponse['_items']);
+    magazineList.total = jsonResponse['_meta']['total'];
+    return magazineList;
+  }
+
+  Future<MagazineList> fetchNextMagazineListPageByType(String type, {int maxResults = 8}) async {
+    _page = _page + 1;
+    MagazineList magazineList = await fetchMagazineListByType(type, page: _page, maxResults: maxResults);
     return magazineList;
   }
 }
