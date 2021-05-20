214441a59a1a6acbe493ef98770d75dfde1f0942
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-05-20 11:18:31 +0800

    add sign in with apple api in login service

--- lib/services/loginService.dart
+++ lib/services/loginService.dart
@@ -3,10 +3,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:readr_app/models/firebaseLoginStatus.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 abstract class LoginRepos {
   Future<FirebaseLoginStatus> signInWithGoogle();
   Future<FirebaseLoginStatus> signInWithFacebook();
+  Future<FirebaseLoginStatus> signInWithApple();
 }
 
 class LoginServices implements LoginRepos{
@@ -93,6 +95,49 @@ class LoginServices implements LoginRepos{
     return FirebaseLoginStatus(
       status: FirebaseStatus.Success,
       message: 'Facebook sign in with firebase success',
+    );
+  }
+
+  // https://stackoverflow.com/questions/62805312/android-sign-in-with-apple-and-firebase-flutter
+  // need a backend server to support apple sign in on android
+  // but we don't support apple sign in on android in this app
+  Future<FirebaseLoginStatus> signInWithApple() async {
+    OAuthCredential oauthCredential;
+    // Trigger the authentication flow
+    try {
+      AuthorizationCredentialAppleID appleCredential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
+
+      oauthCredential = OAuthProvider("apple.com").credential(
+        idToken: appleCredential.identityToken,
+      );
+    } catch (e) {
+      if(e.code == AuthorizationErrorCode.canceled) {
+        return FirebaseLoginStatus(
+          status: FirebaseStatus.Cancel,
+          message: 'Apple sign in cancel',
+        );
+      }
+    }
+
+    try {
+      // Once signed in, get the UserCredential
+      await _auth.signInWithCredential(oauthCredential);
+    } catch(onError) {
+      print('Error sign in with apple $onError');
+      return FirebaseLoginStatus(
+        status: FirebaseStatus.Error,
+        message: onError.toString(),
+      );
+    }
+
+    return FirebaseLoginStatus(
+      status: FirebaseStatus.Success,
+      message: 'Apple sign in with firebase success',
     );
   }
 }
