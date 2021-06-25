7e31e58bbbd5cb1a6531486d46ac699beab2f70a
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-06-25 11:35:56 +0800

    add new search page UI

--- lib/pages/search/searchPage.dart
+++ lib/pages/search/searchPage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:readr_app/blocs/search/bloc.dart';
 import 'package:readr_app/helpers/dataConstants.dart';
+import 'package:readr_app/pages/search/searchWidget.dart';
+import 'package:readr_app/services/searchService.dart';
 
 class SearchPage extends StatefulWidget {
   @override
@@ -11,7 +15,10 @@ class _SearchPageState extends State<SearchPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildBar(context),
-      body: Container(),
+      body: BlocProvider(
+        create: (context) => SearchBloc(searchRepos: SearchServices()),
+        child: SearchWidget(),
+      ),
     );
   }
 
