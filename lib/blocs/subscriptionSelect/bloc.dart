f4378a8a73dec91e3173909a2280a57c371a679f
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-08-18 20:15:05 +0800

    add subscription select bloc

---
+++ lib/blocs/subscriptionSelect/bloc.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:readr_app/blocs/subscriptionSelect/events.dart';
+import 'package:readr_app/blocs/subscriptionSelect/states.dart';
+import 'package:readr_app/services/subscriptionSelectService.dart';
+
+class SubscriptionSelectBloc extends Bloc<SubscriptionSelectEvents, SubscriptionSelectState> {
+  final SubscriptionSelectRepos subscriptionSelectRepos;
+
+  SubscriptionSelectBloc({this.subscriptionSelectRepos}) : super(SubscriptionSelectInitState());
+
+  @override
+  Stream<SubscriptionSelectState> mapEventToState(SubscriptionSelectEvents event) async* {
+    yield* event.run(subscriptionSelectRepos);
+  }
+}
