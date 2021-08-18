75c4a4822065dcf7e1e5b6483d07698d53123117
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-08-18 12:19:13 +0800

    add story bloc in terms of service page

--- lib/pages/termsOfService/mMTermsOfServicePage.dart
+++ lib/pages/termsOfService/mMTermsOfServicePage.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:readr_app/blocs/story/bloc.dart';
 import 'package:readr_app/helpers/dataConstants.dart';
-import 'package:readr_app/models/story.dart';
-import 'package:readr_app/pages/termsOfService/termsOfServiceWidget.dart';
+import 'package:readr_app/pages/termsOfService/mMTermsOfServiceWidget.dart';
+import 'package:readr_app/services/storyService.dart';
 
 class MMTermsOfServicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildBar(context),
-      body: TermsOfServiceWidget(story: Story(),),
+      body: BlocProvider(
+        create: (context) => StoryBloc(storyRepos: StoryService()),
+        child: MMTermsOfServiceWidget(),
+      ),
     );
   }
 
---
+++ lib/pages/termsOfService/mMTermsOfServiceWidget.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:readr_app/blocs/story/bloc.dart';
+import 'package:readr_app/blocs/story/events.dart';
+import 'package:readr_app/blocs/story/states.dart';
+import 'package:readr_app/models/storyRes.dart';
+import 'package:readr_app/pages/termsOfService/termsOfServiceWidget.dart';
+
+class MMTermsOfServiceWidget extends StatefulWidget {
+  @override
+  _MMTermsOfServiceWidgetState createState() => _MMTermsOfServiceWidgetState();
+}
+
+class _MMTermsOfServiceWidgetState extends State<MMTermsOfServiceWidget> {
+  @override
+  void initState() {
+    super.initState();
+    _fetchPublishedStoryBySlug('service-rule');
+  }
+
+  _fetchPublishedStoryBySlug(String storySlug) {
+    context.read<StoryBloc>().add(
+      FetchPublishedStoryBySlug(storySlug)
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<StoryBloc, StoryState>(
+      builder: (BuildContext context, StoryState state) {
+        if (state is StoryError) {
+          final error = state.error;
+          print('StoryError: ${error.message}');
+          return Container();
+        }
+
+        if (state is StoryLoaded) {
+          StoryRes storyRes = state.storyRes;
+
+          return Padding(
+            padding: const EdgeInsets.only(left: 24, right: 24),
+            child: TermsOfServiceWidget(story: storyRes.story,),
+          );
+        }
+
+        // state is Init, Loading
+        return _loadingWidget();
+      }
+    );
+  }
+
+  Widget _loadingWidget() {
+    return Center(child: CircularProgressIndicator(),);
+  }
+}
