53931391b47db97d9db58b4ec13e67fa257b26cd
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-09-09 22:52:26 +0800

    add delete member cubit

---
+++ lib/blocs/memberCenter/deleteMember/state.dart
@@ -0,0 +1,9 @@
+abstract class DeleteMemberState {}
+
+class DeleteMemberInitState extends DeleteMemberState {}
+
+class DeleteMemberLoading extends DeleteMemberState {}
+
+class DeleteMemberSuccess extends DeleteMemberState {}
+
+class DeleteMemberError extends DeleteMemberState {}
