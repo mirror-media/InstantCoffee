2cad5902ec09050c625cf744b1d7a8cca66cf169
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-09-28 15:49:26 +0800

    add appVersionApi in base config

--- lib/models/baseConfig.dart
+++ lib/models/baseConfig.dart
@@ -52,4 +52,6 @@ class BaseConfig {
 
   String get appsFlyerKey => isDev ? dev.appsFlyerKey : prod.appsFlyerKey;
   String get firebaseApiKey => isDev ? dev.firebaseApiKey : prod.firebaseApiKey;
+
+  String get appVersionApi => isDev ? dev.appVersionApi : prod.appVersionApi;
 }
