0dd85042160518cadacee66a4a3a7198aaf96975
samfeng0517 <sam0880517@gmail.com>
2021-10-05 00:42:08 +0800

    Change text judgement

--- lib/pages/memberCenter/subscriptionSelect/hintToWebsitePage.dart
+++ lib/pages/memberCenter/subscriptionSelect/hintToWebsitePage.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:readr_app/helpers/dataConstants.dart';
+import 'package:readr_app/models/memberSubscriptionType.dart';
 
 class HintToWebsitePage extends StatelessWidget {
-  final String titleText;
-  HintToWebsitePage(this.titleText);
+  final SubscritionType subscritionType;
+  HintToWebsitePage(this.subscritionType);
   @override
   Widget build(BuildContext context) {
-    String hintText = '若欲升級 Premium 會員，請至網頁版進行操作。\nAPP 升級功能將於近期上線。';
-    if(titleText == '變更方案'){
+    String hintText;
+    String titleText;
+    if(subscritionType == SubscritionType.subscribe_one_time){
+      titleText = '升級會員';
+      hintText = '若欲升級 Premium 會員，請至網頁版進行操作。\nAPP 升級功能將於近期上線。';
+    }
+    else{
+      titleText = '變更方案';
       hintText = '若欲變更會員訂閱方案，請至網頁版進行操作。\nAPP 變更方案功能將於近期上線。';
     }
     return Scaffold(
-      appBar: _buildBar(context),
+      appBar: _buildBar(context,titleText),
       body: Column(
         children: [
           SizedBox(height: 48),
@@ -20,6 +27,7 @@ class HintToWebsitePage extends StatelessWidget {
             child: Center(
               child: Text(
                 hintText,
+                textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 17),
               ),
             ),
@@ -29,7 +37,7 @@ class HintToWebsitePage extends StatelessWidget {
     );
   }
 
-  Widget _buildBar(BuildContext context) {
+  Widget _buildBar(BuildContext context, String titleText) {
     return AppBar(
       leading: IconButton(
         icon: Icon(Icons.arrow_back_ios),
