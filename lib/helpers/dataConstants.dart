3936193642e1b4933c4c2bbd744f8465a362a468
samfeng0517 <sam0880517@gmail.com>
2021-10-12 12:15:35 +0800

    Add banner jpg

--- lib/helpers/dataConstants.dart
+++ lib/helpers/dataConstants.dart
@@ -34,6 +34,7 @@ const String myBookPng = 'assets/image/magazine/my_book.png';
 const String pubuPng = 'assets/image/magazine/pubu.png';
 const String readmooPng = 'assets/image/magazine/readmoo.png';
 const String subscriptionIconPng = 'assets/image/subscription_icon.png';
+const String subscribeBannerJpg = 'assets/image/subscribe_banner_mobile.jpg';
 
 // online magazine URL
 const String hamiMagazineURL = 'https://bookstore.emome.net/Searchs/finish/keyword:%E9%8F%A1%E9%80%B1%E5%88%8A';
