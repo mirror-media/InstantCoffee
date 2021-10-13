e77cda9f2a4879204f88957ee4a29f973cac0868
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-10-13 14:22:49 +0800

    add listening story cubit

---
+++ lib/blocs/storyPage/listening/cubit.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:readr_app/blocs/storyPage/listening/states.dart';
+import 'package:readr_app/env.dart';
+import 'package:readr_app/helpers/appException.dart';
+import 'package:readr_app/helpers/exceptions.dart';
+import 'package:readr_app/models/listening.dart';
+import 'package:readr_app/models/recordList.dart';
+import 'package:readr_app/models/storyAd.dart';
+import 'package:readr_app/services/listeningTabContentService.dart';
+import 'package:readr_app/services/listeningWidgetService.dart';
+
+class ListeningStoryCubit extends Cubit<ListeningStoryState> {
+  ListeningStoryCubit() : super(ListeningStoryInitState());
+
+  void fetchListeningStoryPageInfo(String slug) async {
+    print('Fetch listening story page info { slug: $slug }');
+    emit(ListeningStoryLoading());
+    try{
+      ListeningWidgetService listeningWidgetService = ListeningWidgetService();
+      ListeningTabContentService listeningTabContentService = ListeningTabContentService();
+      Listening listening = await listeningWidgetService.fetchListening(slug);
+      RecordList recordList = await listeningTabContentService.fetchRecordList(
+          env.baseConfig.apiBase +
+              'youtube/search?maxResults=7&order=date&part=snippet&channelId=UCYkldEK001GxR884OZMFnRw');
+
+      String storyAdJsonFileLocation = Platform.isIOS
+      ? env.baseConfig.iOSStoryAdJsonLocation
+      : env.baseConfig.androidStoryAdJsonLocation;
+      // String storyAdJsonFileLocation = Platform.isIOS
+      // ? 'assets/data/iOSTestStoryAd.json'
+      // : 'assets/data/androidTestStoryAd.json';
+      String storyAdString = await rootBundle.loadString(storyAdJsonFileLocation);
+      final storyAdMaps = json.decode(storyAdString);
+
+      listening.storyAd = StoryAd.fromJson(storyAdMaps['videohub']);
+      
+      emit(
+        ListeningStoryLoaded(
+          listening: listening,
+          recordList: recordList,
+        )
+      );
+      
+    } on SocketException {
+      emit(
+        ListeningStoryError(error: NoInternetException('No Internet'))
+      );
+    } on HttpException {
+      emit(
+        ListeningStoryError(error: NoServiceFoundException('No Service Found'))
+      );
+    } on FormatException {
+      emit(
+        ListeningStoryError(error: InvalidFormatException('Invalid Response format'))
+      );
+    } on FetchDataException {
+      emit(
+        ListeningStoryError(error: NoInternetException('Error During Communication'))
+      );
+    } on BadRequestException {
+      emit(
+        ListeningStoryError(error: Error400Exception('Invalid Request'))
+      );
+    } on UnauthorisedException {
+      emit( 
+        ListeningStoryError(error: Error400Exception('Unauthorised'))
+      );
+    } on InvalidInputException {
+      emit( 
+        ListeningStoryError(error: Error400Exception('Invalid Input'))
+      );
+    } on InternalServerErrorException {
+      emit(
+        ListeningStoryError(error: NoServiceFoundException('Internal Server Error'))
+      );
+    } catch (e) {
+      emit(
+        ListeningStoryError(error: UnknownException(e.toString()))
+      );
+    }
+  }
+}
