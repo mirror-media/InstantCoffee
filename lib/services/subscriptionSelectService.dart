3636d1e4cae6a6f8e2ff3a0223ca8038d8e4826c
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-12-07 21:21:51 +0800

    add fetchAndroidSubscriptionDetail api in SubscriptionSelectServices

--- lib/services/subscriptionSelectService.dart
+++ lib/services/subscriptionSelectService.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
+import 'package:readr_app/helpers/apiBaseHelper.dart';
 import 'package:readr_app/helpers/appException.dart';
 import 'package:readr_app/helpers/environment.dart';
+import 'package:readr_app/models/graphqlBody.dart';
 
 List<String> _kProductIds = <String>[
   Environment().config.monthSubscriptionId,
@@ -9,11 +14,25 @@ List<String> _kProductIds = <String>[
 
 abstract class SubscriptionSelectRepos {
   Future<List<ProductDetails>> fetchProductDetailList();
+  Future<PurchaseDetails> fetchAndroidSubscriptionDetail();
   Future<bool> buySubscriptionProduct(PurchaseParam purchaseParam);
 }
 
 class SubscriptionSelectServices implements SubscriptionSelectRepos{
+  final ApiBaseHelper _helper = ApiBaseHelper();
   final InAppPurchase _inAppPurchase = InAppPurchase.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Map<String,String> getHeaders(String token) {
+    Map<String,String> headers = {
+      "Content-Type": "application/json",
+    };
+    if(token != null) {
+      headers.addAll({"Authorization": "Bearer $token"});
+    }
+
+    return headers;
+  }
 
   @override
   Future<List<ProductDetails>> fetchProductDetailList() async{
@@ -27,9 +46,67 @@ class SubscriptionSelectServices implements SubscriptionSelectRepos{
     throw FetchDataException('The payment platform is unavailable');
   }
 
+  @override
+  Future<PurchaseDetails> fetchAndroidSubscriptionDetail() async{
+    String firebaseId = _auth.currentUser.uid;
+    String token = await _auth.currentUser.getIdToken();
+
+    String query = """
+    query fetchMemberSubscriptions(\$firebaseId: String!) {
+      member(where: { firebaseId: \$firebaseId }) {
+        subscription(
+          orderBy: { createdAt: desc },
+          where: {
+            isActive: true,
+            paymentMethod: google_play
+          },
+          first: 1
+        ) {
+          frequency
+          googlePlayPurchaseToken
+          paymentMethod
+        }
+      }
+    }
+    """;
+    Map<String, String> variables = {"firebaseId": "$firebaseId"};
+    GraphqlBody graphqlBody = GraphqlBody(
+      operationName: '',
+      query: query,
+      variables: variables,
+    );
+
+    final jsonResponse = await _helper.postByUrl(
+      Environment().config.memberApi,
+      jsonEncode(graphqlBody.toJson()),
+      headers: getHeaders(token),
+    );
+
+    PurchaseDetails purchaseDetails = PurchaseDetails(
+      productID: jsonResponse['data']['member']['subscription'][0]['frequency'],
+      verificationData: PurchaseVerificationData(
+        localVerificationData: '',
+        serverVerificationData: jsonResponse['data']['member']['subscription'][0]['googlePlayPurchaseToken'],
+        source: '',
+      ),
+      transactionDate: '',
+      status: PurchaseStatus.purchased,
+    );
+
+    return purchaseDetails;
+  }  
+
 
   @override
   Future<bool> buySubscriptionProduct(PurchaseParam purchaseParam) async{
-    return await _inAppPurchase.buyNonConsumable(purchaseParam: purchaseParam);
-  }  
+    bool buySuccess = false;
+
+    try {
+      buySuccess = await _inAppPurchase.buyNonConsumable(purchaseParam: purchaseParam);
+    } catch(e) {
+      print('buySubscriptionProduct' + e.toString());
+    }
+    
+    return buySuccess;
+  }
 }
