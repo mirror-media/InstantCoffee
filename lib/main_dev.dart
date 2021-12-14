f1917c51777e43ea10d78efec5e0952873162451
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-12-14 16:40:09 +0800

    delete deprecated feature in in_app_purchase_android

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,23 +1,14 @@
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:in_app_purchase_android/in_app_purchase_android.dart';
 import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:readr_app/helpers/environment.dart';
 import 'package:readr_app/mirrorMediaApp.dart';
 
 void main() async{
-  // Inform the plugin that this app supports pending purchases on Android.
-  // An error will occur on Android if you access the plugin `instance`
-  // without this call.
-  if (defaultTargetPlatform == TargetPlatform.android) {
-    InAppPurchaseAndroidPlatformAddition.enablePendingPurchases();
-  }
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   
