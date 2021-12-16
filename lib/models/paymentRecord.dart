18b28404a18c55d63f3546f55bc6aa92edc2bfc6
samfeng0517 <sam0880517@gmail.com>
2021-12-16 16:53:38 +0800

    feat(paymentRecord): Add Payment type enum and change paymentType to subscribeType

--- lib/models/paymentRecord.dart
+++ lib/models/paymentRecord.dart
@@ -1,20 +1,26 @@
-
+enum PaymentType{
+  newebpay,
+  appStore,
+  googlePlay,
+}
 class PaymentRecord {
   final String paymentOrderNumber;
-  final String paymentType;
+  final String subscribeType;
   final String paymentCurrency;
   final int paymentAmount;
   final DateTime paymentDate;
   final String paymentMethod;
   final bool isSuccess;
+  final PaymentType paymentType;
 
   PaymentRecord({
     this.paymentOrderNumber,
-    this.paymentType,
+    this.subscribeType,
     this.paymentCurrency,
     this.paymentAmount,
     this.paymentDate,
     this.paymentMethod,
-    this.isSuccess
+    this.isSuccess,
+    this.paymentType,
   });
 }
