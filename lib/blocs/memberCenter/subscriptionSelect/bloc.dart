c8c06adc56d0f547b9927324f7d6eb4aee78b223
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2021-12-28 18:22:13 +0800

    add showing subscription warning

--- lib/blocs/memberCenter/subscriptionSelect/bloc.dart
+++ lib/blocs/memberCenter/subscriptionSelect/bloc.dart
@@ -7,6 +7,7 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:in_app_purchase/in_app_purchase.dart';
 import 'package:readr_app/blocs/memberCenter/subscriptionSelect/events.dart';
 import 'package:readr_app/blocs/memberCenter/subscriptionSelect/states.dart';
+import 'package:readr_app/helpers/dataConstants.dart';
 import 'package:readr_app/helpers/environment.dart';
 import 'package:readr_app/helpers/exceptions.dart';
 import 'package:readr_app/helpers/iAPSubscriptionHelper.dart';
@@ -42,6 +43,55 @@ class SubscriptionSelectBloc extends Bloc<SubscriptionSelectEvents, Subscription
     _iapSubscriptionHelper.verifyPurchaseInBloc = false;
     _buyingPurchaseSubscription.cancel();
     return super.close();
+  }
+
+  bool _isNeedToShowWarning(SubscritionType subscritionType) {
+    return (subscritionType == SubscritionType.subscribe_monthly || 
+        subscritionType == SubscritionType.subscribe_yearly) &&
+        Platform.isAndroid;
+  }
+
+  void _showWarningDialog() {
+    final ButtonStyle buttonStyle = TextButton.styleFrom(
+      backgroundColor: appColor,
+      padding: const EdgeInsets.only(top: 12, bottom: 12),
+    );
+
+    showDialog(
+      barrierDismissible: false,
+      context: RouteGenerator.navigatorKey.currentContext,
+      builder: (BuildContext context) {
+        double width = MediaQuery.of(context).size.width;
+        double height = MediaQuery.of(context).size.height;
+
+        return AlertDialog(
+          content: Container(
+            height: height/3,
+            child: Column(
+              children: [
+                Expanded(child: Text('若您於 Android 安卓手機操作變更方案，新方案將於新的一期自動生效。新方案生效前，您的頁面仍會顯示原本的訂閱方案，這是正常的，請不用擔心。如有問題可聯繫客服人員。')),
+                OutlinedButton(
+                  style: buttonStyle,
+                  child: Container(
+                    width: width,
+                    child: Center(
+                      child: Text(
+                        '我知道了',
+                        style: TextStyle(
+                          fontSize: 17,
+                          color: Colors.white
+                        ),
+                      ),
+                    ),
+                  ),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 
   _removeSubscribedProduct(
@@ -80,6 +130,10 @@ class SubscriptionSelectBloc extends Bloc<SubscriptionSelectEvents, Subscription
         subscriptionDetail: subscriptionDetail,
         productDetailList: productDetailList,
       ));
+
+      if(_isNeedToShowWarning(subscriptionDetail.subscritionType)) {
+        _showWarningDialog();
+      }
     } on SocketException {
       emit(SubscriptionSelectState.error(
         errorMessages: NoInternetException('No Internet'),
