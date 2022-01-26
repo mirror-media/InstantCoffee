4804057110ab8ccf1c7266dcf4f692f2381a605e
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2022-01-26 20:52:47 +0800

    refactor record model

--- lib/models/record.dart
+++ lib/models/record.dart
@@ -1,13 +1,14 @@
+import 'package:equatable/equatable.dart';
 import 'package:readr_app/helpers/environment.dart';
 import 'package:readr_app/models/category.dart';
 
-class Record {
-  String title;
-  String slug;
-  String publishedDate;
-  String photoUrl;
-  bool isMemberCheck;
-  bool isMemberContent;
+class Record extends Equatable {
+  final String title;
+  final String slug;
+  final String publishedDate;
+  final String photoUrl;
+  final bool isMemberCheck;
+  final bool isMemberContent;
 
   Record({
     required this.title,
@@ -40,27 +41,23 @@ class Record {
 
     String origPublishedDate;
     if (json.containsKey('publishedDate')) {
-      origPublishedDate = json['publishedDate'];
+      origPublishedDate = json['publishedDate'] ?? '';
     } else {
       origPublishedDate = json['publishTime'] ?? '';
     }
 
     String photoUrl = Environment().config.mirrorMediaNotImageUrl;
-    if (json.containsKey('heroImage') && json['heroImage'] != null && json['heroImage']['image'] != null) {
+    if (json['heroImage'] != null && json['heroImage']['image'] != null) {
       photoUrl = json['heroImage']['image']['resizedTargets']['mobile']['url'];
-    } else if (json.containsKey('snippet') && json['snippet'] != null) {
+    } else if (json['snippet'] != null && json['snippet']['thumbnails'] != null) {
       photoUrl = json['snippet']['thumbnails']['medium']['url'];
-    } else if (json.containsKey('photoUrl') && json['photoUrl'] != null) {
+    } else if (json['photoUrl'] != null) {
       photoUrl = json['photoUrl'];
     }
     
-    List<Category> categoryBuilder = [];
-    if (json["categories"] != null) {
-      for (int i = 0; i < json["categories"].length; i++) {
-        Category category = Category.fromJson(json["categories"][i]);
-        categoryBuilder.add(category);
-      }
-    }
+    List<Category> categoryBuilder = json["categories"] == null
+    ? []
+    : Category.categoryListFromJson(json["categories"]);
 
     bool isMemberContent = false;
     if(json["sections"] != null){
@@ -76,9 +73,7 @@ class Record {
       slug: origSlug,
       publishedDate: origPublishedDate,
       photoUrl: photoUrl,
-      isMemberCheck: categoryBuilder.length == 0
-      ? true
-      : Category.isMemberOnlyInCategoryList(categoryBuilder),
+      isMemberCheck: Category.isMemberOnlyInCategoryList(categoryBuilder),
       isMemberContent: isMemberContent,
     );
   }
@@ -90,15 +85,6 @@ class Record {
         'photoUrl': photoUrl,
       };
 
-  @override
-  int get hashCode => this.hashCode;
-
-  @override
-  bool operator ==(covariant Record other) {
-    // compare this to other
-    return this.slug == other.slug;
-  }
-
   static List<Record> recordListFromJson(List<dynamic> jsonList) {
     return jsonList.map<Record>((json) => Record.fromJson(json)).toList();
   }
@@ -109,10 +95,20 @@ class Record {
   ) {
     List<Record> records = [];
     base.forEach((element) { 
-      if(!another.contains(element)) {
+      if(!another.any((record) => record.slug == element.slug)) {
         records.add(element);
       }
     });
     return records;
   }
+
+  @override
+  List<Object?> get props => [
+    title, 
+    slug, 
+    publishedDate, 
+    photoUrl, 
+    isMemberCheck, 
+    isMemberContent
+  ];
 }
