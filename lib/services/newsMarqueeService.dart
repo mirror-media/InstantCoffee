f3ff7e825f3fb72cfc49190d9525f036436a5517
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2022-01-27 23:41:54 +0800

    add NewsMarqueeService test

--- lib/services/newsMarqueeService.dart
+++ lib/services/newsMarqueeService.dart
@@ -3,10 +3,10 @@ import 'package:readr_app/helpers/apiBaseHelper.dart';
 import 'package:readr_app/models/record.dart';
 
 class NewsMarqueeService {
-  ApiBaseHelper _helper = ApiBaseHelper();
+  ApiBaseHelper helper = ApiBaseHelper();
 
   Future<List<Record>> fetchRecordList() async {
-    final jsonResponse = await _helper.getByUrl(Environment().config.newsMarqueeApi);
+    final jsonResponse = await helper.getByUrl(Environment().config.newsMarqueeApi);
 
     List<Record> records = Record.recordListFromJson(jsonResponse["_items"]);
     return records;
