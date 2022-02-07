ecfce53dc92ee81392e8906a36bcd8ba4a6434ca
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2022-02-07 17:42:39 +0800

    extends Equatable in Section model

--- lib/models/section.dart
+++ lib/models/section.dart
@@ -1,13 +1,15 @@
+import 'package:equatable/equatable.dart';
 import 'package:readr_app/models/sectionAd.dart';
 
-class Section {
-  String key;
-  String name;
-  String title;
-  String description;
-  bool focus;
-  int order;
-  String type;
+// ignore: must_be_immutable
+class Section extends Equatable {
+  final String key;
+  final String name;
+  final String title;
+  final String description;
+  final bool focus;
+  final int order;
+  final String type;
 
   SectionAd? sectionAd;
 
@@ -24,37 +26,40 @@ class Section {
   });
 
   factory Section.fromJson(Map<String, dynamic> json) {
-    String type;
-    if (json.containsKey("type") == true) {
-      type = json["type"];
-    } else {
-      type = "section";
-    }
     return Section(
       key: json["_id"] == null ? json["key"] : json["_id"],
       name: json["name"],
       title: json["title"],
       description: json["description"] ?? "",
       order: json["sortOrder"] == null ? json["order"] : json["sortOrder"],
-      focus: false,
-      type: type,
+      focus: json['focus'] ?? false,
+      type: json["type"] ?? "section",
     );
   }
 
   Map<String, dynamic> toJson() => {
+        'key': key,
         'name': name,
         'title': title,
-        'order': order,
-        'key': key,
-        'type': type,
         'description': description,
+        'order': order,
+        'focus': focus,
+        'type': type,
       };
-
-  void setOrder(int newOrder) {
-    this.order = newOrder;
-  }
 
   static List<Section> sectionListFromJson(List<dynamic> jsonList) {
     return jsonList.map<Section>((json) => Section.fromJson(json)).toList();
   }
+
+  @override
+  List<Object?> get props => [
+    key, 
+    name, 
+    title, 
+    description, 
+    focus, 
+    order,
+    type,
+    sectionAd
+  ];
 }
