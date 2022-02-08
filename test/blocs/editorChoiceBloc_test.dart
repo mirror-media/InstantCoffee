d470532d6f887d5a8fa5d13f65e2e779e2faeb03
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2022-02-08 12:43:02 +0800

    add editor choice bloc test

---
+++ test/blocs/editorChoiceBloc_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:readr_app/blocs/editorChoice/cubit.dart';
+import 'package:readr_app/blocs/editorChoice/state.dart';
+import 'package:readr_app/models/record.dart';
+import 'package:readr_app/services/editorChoiceService.dart';
+
+class EditorChoiceTestService implements EditorChoiceRepos{
+  @override
+  Future<List<Record>> fetchRecordList() async{
+    List<Record> result = [];
+    return result;
+  }
+}
+
+void main() {
+  late EditorChoiceCubit editorChoiceCubit;
+
+  setUp(() {
+    editorChoiceCubit = EditorChoiceCubit(editorChoiceRepos: EditorChoiceTestService());
+  });
+
+  group('Editor choice bloc:', () {
+    test('initial state is correct', () {
+      expect(editorChoiceCubit.state, EditorChoiceState.init());
+    });
+
+    test('fetch editor choice record list', () {
+      final expectedResponse = [
+        EditorChoiceState.loading(),
+        EditorChoiceState.loaded(editorChoiceList: []),
+      ];
+
+      expectLater(
+        editorChoiceCubit.stream,
+        emitsInOrder(expectedResponse),
+      );
+      
+      editorChoiceCubit.fetchEditorChoiceRecordList();
+    });
+  });
+}
