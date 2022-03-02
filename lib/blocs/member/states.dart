e823b767a442986071d602b00b25a38756c3f07c
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2022-03-02 20:00:20 +0800

    adds member bloc

---
+++ lib/blocs/member/states.dart
@@ -0,0 +1,72 @@
+import 'package:readr_app/models/memberSubscriptionType.dart';
+
+enum MemberStatus { 
+  initial,
+  loading,
+  loaded,
+  error 
+}
+
+class MemberState {
+  final MemberStatus status;
+  final bool? isLogin;
+  final String? israfelId;
+  final SubscriptionType? subscriptionType;
+  final dynamic errorMessages;
+
+  const MemberState({
+    required this.status,
+    this.isLogin,
+    this.israfelId,
+    this.subscriptionType,
+    this.errorMessages,
+  });
+
+  factory MemberState.init() {
+    return MemberState(
+      status: MemberStatus.initial,
+    );
+  }
+
+  factory MemberState.isNotLogin() {
+    return MemberState(
+      status: MemberStatus.loaded,
+      isLogin: false,
+    );
+  }
+
+
+  factory MemberState.loading() {
+    return MemberState(
+      status: MemberStatus.loading
+    );
+  }
+
+  factory MemberState.loaded({
+    required bool isLogin,
+    required String? israfelId,
+    required SubscriptionType? subscriptionType,
+  }) {
+    return MemberState(
+      status: MemberStatus.loaded,
+      isLogin: isLogin,
+      israfelId: israfelId,
+      subscriptionType: subscriptionType,
+    );
+  }
+
+  factory MemberState.error({
+    dynamic errorMessages
+  }) {
+    return MemberState(
+      status: MemberStatus.error,
+      isLogin: false,
+      errorMessages: errorMessages,
+    );
+  }
+
+  bool get isPremium => 
+      this.isLogin! && 
+      this.subscriptionType != SubscriptionType.none &&
+      this.subscriptionType != SubscriptionType.subscribe_one_time;
+}
