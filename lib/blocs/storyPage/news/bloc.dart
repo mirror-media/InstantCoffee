7ecf20e2ef6fd69790e5ad4003c0e72f28a9db1b
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2022-03-11 14:57:18 +0800

    refactor story bloc

--- lib/blocs/storyPage/news/bloc.dart
+++ lib/blocs/storyPage/news/bloc.dart
@@ -6,7 +6,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:readr_app/blocs/storyPage/news/events.dart';
 import 'package:readr_app/blocs/storyPage/news/states.dart';
 import 'package:readr_app/helpers/environment.dart';
-import 'package:readr_app/helpers/appException.dart';
 import 'package:readr_app/helpers/errorLogHelper.dart';
 import 'package:readr_app/helpers/exceptions.dart';
 import 'package:readr_app/models/story.dart';
@@ -15,138 +14,68 @@ import 'package:readr_app/models/storyRes.dart';
 import 'package:readr_app/services/storyService.dart';
 
 class StoryBloc extends Bloc<StoryEvents, StoryState> {
-  String storySlug;
   final StoryRepos storyRepos;
-
   StoryBloc({
-    required this.storySlug,
+    required String storySlug,
     required this.storyRepos
-  }) : super(StoryState.init());
+  }) : super(StoryState.init(storySlug: storySlug)) {
+    on<FetchPublishedStoryBySlug>(_fetchPublishedStoryBySlug);
+  }
 
-  @override
-  Stream<StoryState> mapEventToState(StoryEvents event) async* {
-    ErrorLogHelper _errorLogHelper = ErrorLogHelper();
-    
-    if(event is FetchPublishedStoryBySlug) {
-      print(event.toString());
-      storySlug = event.slug;
-      try{
-        yield StoryState.loading();
+  ErrorLogHelper _errorLogHelper = ErrorLogHelper();
 
-        StoryRes storyRes = await storyRepos.fetchStory(
-          event.slug, 
-          event.isMemberCheck
-        );
-        Story story = storyRes.story;
+  _fetchPublishedStoryBySlug(
+    FetchPublishedStoryBySlug event,
+    Emitter<StoryState> emit,
+  ) async{
+    print(event.toString());
+
+    try{
+      emit(StoryState.loading(storySlug: event.slug));
+
+      StoryRes storyRes = await storyRepos.fetchStory(
+        event.slug, 
+        event.isMemberCheck
+      );
+      Story story = storyRes.story;
+      
+      String storyAdJsonFileLocation = Platform.isIOS
+      ? Environment().config.iOSStoryAdJsonLocation
+      : Environment().config.androidStoryAdJsonLocation;
+      // String storyAdJsonFileLocation = Platform.isIOS
+      // ? 'assets/data/iOSTestStoryAd.json'
+      // : 'assets/data/androidTestStoryAd.json';
+      String storyAdString = await rootBundle.loadString(storyAdJsonFileLocation);
+      final storyAdMaps = json.decode(storyAdString);
+
+      story.storyAd = StoryAd.fromJson(storyAdMaps['other']);
+      for(int i=0; i<story.sections.length; i++) {
+        String? sectionName = story.getSectionName();
         
-        String storyAdJsonFileLocation = Platform.isIOS
-        ? Environment().config.iOSStoryAdJsonLocation
-        : Environment().config.androidStoryAdJsonLocation;
-        // String storyAdJsonFileLocation = Platform.isIOS
-        // ? 'assets/data/iOSTestStoryAd.json'
-        // : 'assets/data/androidTestStoryAd.json';
-        String storyAdString = await rootBundle.loadString(storyAdJsonFileLocation);
-        final storyAdMaps = json.decode(storyAdString);
-
-        story.storyAd = StoryAd.fromJson(storyAdMaps['other']);
-        for(int i=0; i<story.sections.length; i++) {
-          String? sectionName = story.getSectionName();
-          
-          if(sectionName != null && storyAdMaps[sectionName] != null) {
-            story.storyAd = StoryAd.fromJson(storyAdMaps[sectionName]);
-            break;
-          }
+        if(sectionName != null && storyAdMaps[sectionName] != null) {
+          story.storyAd = StoryAd.fromJson(storyAdMaps[sectionName]);
+          break;
         }
-
-        yield StoryState.loaded(
-          storyRes: storyRes,
-        );
-      } on SocketException {
-        _errorLogHelper.record(
-          event.eventName(),
-          event.eventParameters(),
-          'No Internet',
-        );
-        yield StoryState.error(
-          errorMessages: NoInternetException('No Internet'),
-        );
-      } on HttpException {
-        _errorLogHelper.record(
-          event.eventName(),
-          event.eventParameters(),
-          'No Service Found',
-        );
-        yield StoryState.error(
-          errorMessages: NoServiceFoundException('No Service Found'),
-        );
-      } on FormatException {
-        _errorLogHelper.record(
-          event.eventName(),
-          event.eventParameters(),
-          'Invalid Response format',
-        );
-        yield StoryState.error(
-          errorMessages: InvalidFormatException('Invalid Response format'),
-        );
-      } on FetchDataException {
-        _errorLogHelper.record(
-          event.eventName(),
-          event.eventParameters(),
-          'Error During Communication'
-        );
-        yield StoryState.error(
-          errorMessages: NoInternetException('Error During Communication'),
-        );
-      } on BadRequestException {
-        _errorLogHelper.record(
-          event.eventName(),
-          event.eventParameters(),
-          'Invalid Request',
-        );
-        yield StoryState.error(
-          errorMessages: Error400Exception('Invalid Request'),
-        );
-      } on UnauthorisedException {
-        _errorLogHelper.record(
-          event.eventName(),
-          event.eventParameters(),
-          'Unauthorised',
-        );
-        yield StoryState.error(
-          errorMessages: Error400Exception('Unauthorised'),
-        );
-      } on InvalidInputException {
-        _errorLogHelper.record(
-          event.eventName(),
-          event.eventParameters(),
-          'Invalid Input',
-        );
-        yield StoryState.error(
-          errorMessages: Error400Exception('Invalid Input'),
-        );
-      } on InternalServerErrorException {
-        _errorLogHelper.record(
-          event.eventName(),
-          event.eventParameters(),
-          'Internal Server Error',
-        );
-        yield StoryState.error(
-          errorMessages: NoServiceFoundException('Internal Server Error'),
-        );
-      } catch (e) {
-        _errorLogHelper.record(
-          event.eventName(),
-          event.eventParameters(),
-          e.toString(),
-        );
-        yield StoryState.error(
-          errorMessages: UnknownException(e.toString()),
-        );
       }
+
+      emit(StoryState.loaded(
+        storySlug: event.slug, 
+        storyRes: storyRes
+      ));
+    } catch (e) {
+      _errorLogHelper.record(
+        event.eventName(),
+        event.eventParameters(),
+        e.toString(),
+      );
+      emit(StoryState.error(
+        storySlug: event.slug, 
+        errorMessages: UnknownException(e.toString())
+      ));
     }
   }
 
   String getShareUrlFromSlug() {
-    return '${Environment().config.mirrorMediaDomain}/story/$storySlug/?utm_source=app&utm_medium=mmapp';
+    return '${Environment().config.mirrorMediaDomain}/story/${state.storySlug}/?utm_source=app&utm_medium=mmapp';
   }
 }
--- lib/blocs/storyPage/news/states.dart
+++ lib/blocs/storyPage/news/states.dart
@@ -4,47 +4,59 @@ enum StoryStatus { initial, loading, loaded, error }
 
 class StoryState {
   final StoryStatus status;
+  final String storySlug;
   final StoryRes? storyRes;
   final errorMessages;
 
   const StoryState({
     required this.status,
+    required this.storySlug,
     this.storyRes,
     this.errorMessages,
   });
 
-  factory StoryState.init() {
+  factory StoryState.init({
+    required String storySlug
+  }) {
     return StoryState(
-      status: StoryStatus.initial
+      status: StoryStatus.initial,
+      storySlug: storySlug
     );
   }
 
-  factory StoryState.loading() {
+  factory StoryState.loading({
+    required String storySlug
+  }) {
     return StoryState(
-      status: StoryStatus.loading
+      status: StoryStatus.loading,
+      storySlug: storySlug
     );
   }
 
   factory StoryState.loaded({
+    required String storySlug,
     required StoryRes storyRes,
   }) {
     return StoryState(
       status: StoryStatus.loaded,
+      storySlug: storySlug,
       storyRes: storyRes,
     );
   }
 
   factory StoryState.error({
+    required String storySlug,
     errorMessages
   }) {
     return StoryState(
       status: StoryStatus.error,
+      storySlug: storySlug,
       errorMessages: errorMessages,
     );
   }
 
   @override
   String toString() {
-    return 'StoryState { status: $status }';
+    return 'StoryState { status: $status, storySlug: $storySlug }';
   }
 }
