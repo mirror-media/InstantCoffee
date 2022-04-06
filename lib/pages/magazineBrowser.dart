980adf3364424722fba28f6cfa9924c8f265ca9d
HiIamAlanOu <50572272+HiIamAlanOu@users.noreply.github.com>
2022-04-06 16:18:14 +0800

    adds device orientation left and right in MagazineBrowser

--- lib/pages/magazineBrowser.dart
+++ lib/pages/magazineBrowser.dart
@@ -1,49 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 import 'package:readr_app/helpers/dataConstants.dart';
 import 'package:readr_app/models/magazine.dart';
 
-class MagazineBrowser extends StatelessWidget {
+class MagazineBrowser extends StatefulWidget {
   final Magazine magazine;
   MagazineBrowser({
     required this.magazine,
   });
-  
+
   @override
-  Widget build(BuildContext context) {
-    Widget browser;
+  State<MagazineBrowser> createState() => _MagazineBrowserState();
+}
+
+class _MagazineBrowserState extends State<MagazineBrowser> {
+  late Widget _browser;
+
+  void _setBrowser(String type) {
     InAppWebViewGroupOptions options = InAppWebViewGroupOptions(
       crossPlatform: InAppWebViewOptions(
-        disableContextMenu: magazine.type == 'weekly',
+        disableContextMenu: type == 'weekly',
       ),
       android: AndroidInAppWebViewOptions(
         useHybridComposition: true,
-        allowContentAccess: magazine.type != 'weekly',
+        allowContentAccess: type != 'weekly',
       ),
       ios: IOSInAppWebViewOptions(
-        allowsLinkPreview: magazine.type != 'weekly',
-        disableLongPressContextMenuOnLinks: magazine.type == 'weekly',
+        allowsLinkPreview: type != 'weekly',
+        disableLongPressContextMenuOnLinks: type == 'weekly',
       ),
     );
 
-    if(magazine.type == 'weekly'){
-      browser = InAppWebView(
+    if(type == 'weekly'){
+      _browser = InAppWebView(
         initialUrlRequest: URLRequest(
-          url: Uri.parse(magazine.onlineReadingUrl),
+          url: Uri.parse(widget.magazine.onlineReadingUrl),
         ),
         initialOptions: options,
       );
-    }else{
-      browser = InAppWebView(
+    } else{
+      _browser = InAppWebView(
         initialUrlRequest: URLRequest(
-          url: Uri.parse(magazine.pdfUrl),
+          url: Uri.parse(widget.magazine.pdfUrl),
         ),
         initialOptions: options,
       );
     }
-    return Scaffold(
-      appBar: _buildBar(context),
-      body: browser,
+  }
+
+  @override
+  void initState() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+
+    _setBrowser(widget.magazine.type);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return OrientationBuilder(
+      builder: (context, orientation) {
+        if(orientation  == Orientation.portrait) {
+          return Scaffold(
+            appBar: _buildBar(context),
+            body: _browser,
+          );
+        }
+        return Scaffold(
+          body: Stack(
+            children: [
+              _browser,
+              SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8.0),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: InkWell(
+                      child: Container(
+                        color: Colors.black45,
+                        child: Icon(
+                          Icons.close,
+                          color: Colors.white,
+                          size: 36,
+                        ),
+                      ),
+                      onTap: () => Navigator.of(context).pop()
+                    ),             
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
     );
   }
 
@@ -51,10 +115,10 @@ class MagazineBrowser extends StatelessWidget {
     return AppBar(
       leading: IconButton(
         icon: Icon(Icons.arrow_back_ios),
-        onPressed: () => Navigator.of(context).pop(),
+        onPressed: () => Navigator.of(context).pop()
       ),
       centerTitle: true,
-      title: Text(magazine.issue),
+      title: Text(widget.magazine.issue),
       backgroundColor: appColor,
     );
   }
