9d429abb12e14a98df47b5a08e316148130f823c
Sam Feng <sam0880517@gmail.com>
2022-05-16 14:42:29 +0800

    feat(topic): add field for group

--- lib/models/topic.dart
+++ lib/models/topic.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:csslib/parser.dart' as csslib;
+import 'package:csslib/visitor.dart';
 
 enum TopicType {
   list,
@@ -15,6 +17,10 @@ class Topic {
   final String? ogImageUrl;
   final Color bgColor;
   final List<String>? tagIdList;
+  final Map<String, int>? tagOrderMap;
+  final Color subTitleColor;
+  final Color recordTitleColor;
+  final Color dividerColor;
 
   const Topic({
     required this.id,
@@ -25,6 +31,10 @@ class Topic {
     this.ogImageUrl,
     this.bgColor = Colors.white,
     this.tagIdList,
+    this.subTitleColor = Colors.white,
+    this.recordTitleColor = Colors.black,
+    this.dividerColor = Colors.grey,
+    this.tagOrderMap,
   });
 
   factory Topic.fromJson(Map<String, dynamic> json) {
@@ -43,18 +53,54 @@ class Topic {
     }
 
     Color bgColor = Colors.white;
-    if (json.containsKey('style') && type != TopicType.list) {
+    Color subTitleColor = Colors.white;
+    Color recordTitleColor = Colors.black;
+    Color dividerColor = Colors.grey;
+    Map<String, int> tagOrderMap = {};
+    if (json.containsKey('style') && type == TopicType.group) {
       String styleString = json['style'] as String;
-      if (styleString.contains('文章區塊背景色')) {
-        //find background color text and convert to color
-        const start = "文章區塊背景色 */\r\n  background-color:";
-        const end = "!";
-        final startIndex = styleString.indexOf(start);
-        final endIndex = styleString.indexOf(end, startIndex + start.length);
-        String hexString =
-            styleString.substring(startIndex + start.length, endIndex);
-        String colorString = hexString.replaceAll('#', '0xff');
-        bgColor = Color(int.parse(colorString));
+
+      StyleSheet css = csslib.parse(styleString);
+
+      for (int i = 0; i < css.topLevels.length; i++) {
+        var item = css.topLevels[i];
+        if (item is RuleSet) {
+          SelectorGroup? selectorGroup = item.selectorGroup;
+          if (selectorGroup != null) {
+            List<Selector> selectors = selectorGroup.selectors;
+            List<SimpleSelectorSequence> simpleSelectors =
+                selectors[0].simpleSelectorSequences;
+            SimpleSelector simpleSelector = simpleSelectors[0].simpleSelector;
+            // find background color
+            if (simpleSelector.name == 'groupList') {
+              bgColor = _findColorInCss(item);
+            } else if (simpleSelector.name == 'groupListBlock') {
+              // find divider color
+              dividerColor = _findColorInCss(item);
+            } else if (simpleSelector.name == 'groupListBlockContainer') {
+              //find subtitle and record title text color
+              simpleSelector = simpleSelectors[1].simpleSelector;
+              if (simpleSelector.name == 'h1' && simpleSelectors.length == 2) {
+                subTitleColor = _findColorInCss(item);
+                recordTitleColor = subTitleColor;
+              } else if (simpleSelector.name.contains('tag')) {
+                String tagId = simpleSelector.name.replaceAll('tag-', '');
+                int tagOrder = _findOrderInCss(item);
+                tagOrderMap.putIfAbsent(tagId, () => tagOrder);
+              }
+            }
+          }
+        }
+      }
+    }
+
+    List<String>? tagIdList;
+    if (json.containsKey('tags') &&
+        type == TopicType.group &&
+        json['tags'].isNotEmpty) {
+      tagIdList = [];
+      for (var tagId in json['tags']) {
+        tagIdList.add(tagId);
       }
     }
 
@@ -66,6 +112,29 @@ class Topic {
       ogImageUrl: ogImageUrl,
       type: type,
       bgColor: bgColor,
+      tagIdList: tagIdList,
+      subTitleColor: subTitleColor,
+      recordTitleColor: recordTitleColor,
+      dividerColor: dividerColor,
+      tagOrderMap: tagOrderMap,
     );
+  }
+
+  static Color _findColorInCss(RuleSet ruleSet) {
+    Declaration declaration =
+        ruleSet.declarationGroup.declarations[0] as Declaration;
+    Expressions expressions = declaration.expression as Expressions;
+    HexColorTerm hexColorTerm = expressions.expressions[0] as HexColorTerm;
+    String colorString = '0xff' + hexColorTerm.text;
+    return Color(int.parse(colorString));
+  }
+
+  static int _findOrderInCss(RuleSet ruleSet) {
+    Declaration declaration =
+        ruleSet.declarationGroup.declarations[0] as Declaration;
+    Expressions expressions = declaration.expression as Expressions;
+    NumberTerm numberTerm = expressions.expressions[0] as NumberTerm;
+    String orderString = numberTerm.text;
+    return int.parse(orderString);
   }
 }
