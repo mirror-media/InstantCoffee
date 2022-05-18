fef31536bb4dca21a42dae7b718580f3259282e9
Sam Feng <sam0880517@gmail.com>
2022-05-18 16:17:04 +0800

    refactor(topicPage): remove unused import

--- lib/pages/topic/topicPage.dart
+++ lib/pages/topic/topicPage.dart
@@ -1,5 +1,4 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:readr_app/controllers/topic/topicPageController.dart';
