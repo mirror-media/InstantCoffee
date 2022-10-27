fdfa6fbb252446ff921fcb3210666762ceebdd3d
Sam Feng <sam0880517@gmail.com>
2022-10-27 15:16:00 +0800

    feat(bloc): create election cubit and state

---
+++ lib/blocs/election/election_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:readr_app/models/election/municipality.dart';
+import 'package:readr_app/services/electionService.dart';
+
+part 'election_state.dart';
+
+class ElectionCubit extends Cubit<ElectionState> {
+  final ElectionRepos repos;
+  ElectionCubit(this.repos) : super(ElectionInitial());
+
+  void fetchMunicipalityData() async {
+    emit(UpdatingElectionData());
+    try {
+      var result = await repos.fetchMunicipalityData();
+      emit(ElectionDataLoaded(
+        lastUpdateTime: result['lastUpdateTime'],
+        municipalityList: result['municipalityList'],
+      ));
+    } catch (e) {
+      print('Fetch election data failed: $e');
+      emit(ElectionDataError());
+    }
+  }
+}
