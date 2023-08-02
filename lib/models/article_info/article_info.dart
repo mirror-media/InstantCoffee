84748c4b2562a560bd4236815ced083215c9635c
Mike <mikelu@mirrormedia.mg>
2023-08-02 21:13:30 +0800

    Refactor(pubspec.yaml): 版本調整

--- lib/models/article_info/article_info.dart
+++ lib/models/article_info/article_info.dart
@@ -1,13 +1,21 @@
+
+import 'package:flutter/material.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:readr_app/models/article_info/children_model/article_tag.dart';
+import 'package:readr_app/data/enum/author_occupation.dart';
+import 'package:readr_app/models/article_info/children_model/brief/brief.dart';
+import 'package:readr_app/models/author_occupation.dart';
+import 'package:readr_app/models/paragraph.dart';
+
+
 
 import 'package:readr_app/models/post/post_model.dart';
 
+import '../../helpers/data_constants.dart';
 import '../../helpers/environment.dart';
 import '../story_ad.dart';
-import 'children_model/brief.dart';
 import 'children_model/category.dart';
-import 'children_model/hero_image.dart';
+import 'children_model/hero_image/hero_image.dart';
 import 'children_model/people.dart';
 import 'children_model/section.dart';
 
@@ -31,7 +39,7 @@ class ArticleInfo {
   List<People>? cameraMan;
   List<People>? designers;
   List<People>? engineers;
-  Brief? brief;
+  List<Paragraph>? brief;
   List<Category>? categories;
   List<Section>? sections;
   bool? isMember;
@@ -68,4 +76,69 @@ class ArticleInfo {
   String? get shareUrl => slug != null
       ? '${Environment().config.mirrorMediaDomain}/story/$slug/?utm_source=app&utm_medium=mmapp'
       : null;
+
+  Color? get sectionColor {
+    if (sectionName != null && sectionColorMaps.containsKey(sectionName)) {
+      return Color(sectionColorMaps[sectionName]!);
+    }
+    return null;
+  }
+
+  String? get sectionName {
+    if (sections != null) {
+      if (sections!.contains('member')) {
+        return 'member';
+      }
+
+      if (sections!.isNotEmpty) {
+        return sections![0].name;
+      }
+    }
+    return null;
+  }
+
+  List<AuthorOccupationModel> get authorList {
+    List<AuthorOccupationModel> authorList = [];
+    addPeopleListToAuthorList(authorList, writers, AuthorOccupation.writer);
+    addPeopleListToAuthorList(
+        authorList, photographers, AuthorOccupation.photographer);
+    addPeopleListToAuthorList(
+        authorList, cameraMan, AuthorOccupation.cameraMan);
+    addPeopleListToAuthorList(authorList, designers, AuthorOccupation.designer);
+    addPeopleListToAuthorList(authorList, engineers, AuthorOccupation.engineer);
+    if (extendByline != null) {
+      authorList
+          .add(AuthorOccupationModel(AuthorOccupation.writer, extendByline!));
+    }
+    return authorList;
+  }
+
+  List<String> get categoryDisplayList {
+    List<String> categoryDisplayList = [];
+
+    if (sections != null && sections!.contains('member')) {
+      categoryDisplayList.add('會員專區');
+    }
+    for (var section in sections!) {
+      if (section.name != null) {
+        categoryDisplayList.add(section.name!);
+      }
+    }
+
+    for (var category in categories!) {
+      if (category.name != null) {
+        categoryDisplayList.add(category.name!);
+      }
+    }
+    return categoryDisplayList;
+  }
+
+  void addPeopleListToAuthorList(List<AuthorOccupationModel> authorList,
+      List<People>? peopleList, AuthorOccupation authorOccupation) {
+    if (peopleList != null && peopleList.isNotEmpty) {
+      authorList.addAll(peopleList
+          .map((people) => AuthorOccupationModel(authorOccupation, people.name))
+          .toList());
+    }
+  }
 }
