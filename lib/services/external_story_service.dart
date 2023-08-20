3976ba7182c07638a057862aa638376543fd41cd
Mike <mikelu@mirrormedia.mg>
2023-08-21 01:06:59 +0800

    Refactor(External):API K3->k6轉換

--- lib/services/external_story_service.dart
+++ lib/services/external_story_service.dart
@@ -1,5 +1,5 @@
-import 'package:readr_app/helpers/api_base_helper.dart';
-import 'package:readr_app/helpers/environment.dart';
+import 'package:get/get.dart';
+import 'package:readr_app/data/providers/articles_api_provider.dart';
 import 'package:readr_app/models/external_story.dart';
 
 abstract class ExternalStoryRepos {
@@ -7,15 +7,11 @@ abstract class ExternalStoryRepos {
 }
 
 class ExternalStoryService implements ExternalStoryRepos {
-  final ApiBaseHelper _helper = ApiBaseHelper();
+
+  final ArticlesApiProvider articlesApiProvider =Get.find ();
 
   @override
   Future<ExternalStory> fetchStory(String slug) async {
-    String url =
-        '${Environment().config.externalStoryPageApi}?where={"name":{"\$in":["$slug"]}}';
-    dynamic jsonResponse = await _helper.getByUrl(url);
-    ExternalStory externalStory =
-        ExternalStory.fromJson(jsonResponse['_items'][0]);
-    return externalStory;
+    return await articlesApiProvider.getExternalArticleBySlug(slug: slug);
   }
 }
