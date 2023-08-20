c26f0d01c91f71dda03ed8e45f04a6770d5cd60e
Mike <mikelu@mirrormedia.mg>
2023-08-21 01:13:53 +0800

    Refactor(Category Page):API K3->k6轉換

--- lib/blocs/personalPage/category/bloc.dart
+++ lib/blocs/personalPage/category/bloc.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:localstorage/localstorage.dart';
-import 'package:readr_app/blocs/personalPage/category/states.dart';
+import 'package:get/get.dart';
 import 'package:readr_app/blocs/personalPage/category/events.dart';
+import 'package:readr_app/blocs/personalPage/category/states.dart';
+import 'package:readr_app/data/providers/articles_api_provider.dart';
+import 'package:readr_app/data/providers/local_storage_provider.dart';
 import 'package:readr_app/models/category.dart';
 import 'package:readr_app/services/category_service.dart';
 import 'package:readr_app/widgets/logger.dart';
@@ -9,6 +11,7 @@ import 'package:readr_app/widgets/logger.dart';
 class PersonalCategoryBloc
     extends Bloc<PersonalCategoryEvents, PersonalCategoryState> with Logger {
   final CategoryRepos categoryRepos;
+
   PersonalCategoryBloc({
     required this.categoryRepos,
   }) : super(PersonalCategoryState.init()) {
@@ -16,32 +19,29 @@ class PersonalCategoryBloc
     on<SetCategoryListInStorage>(_setCategoryListInStorage);
   }
 
-  final LocalStorage _storage = LocalStorage('setting');
+  final ArticlesApiProvider articlesApiProvider = Get.find();
+  final LocalStorageProvider localStorageProvider = Get.find();
 
   void _fetchSubscribedCategoryList(
     FetchSubscribedCategoryList event,
     Emitter<PersonalCategoryState> emit,
   ) async {
     debugLog(event.toString());
+
     try {
       emit(PersonalCategoryState.subscribedCategoryListLoading());
-
-      List<Category>? localCategoryList;
+      List<Category>? localCategoryList =
+          await localStorageProvider.loadCategoryList();
       List<Category> onlineCategoryList =
-          await categoryRepos.fetchCategoryList();
-      if (await _storage.ready) {
-        if (_storage.getItem("categoryList") != null) {
-          localCategoryList =
-              Category.categoryListFromJson(_storage.getItem("categoryList"));
-        }
+          await articlesApiProvider.getCategoriesList();
+
+      if (localCategoryList == null || localCategoryList.isEmpty) {
+        localStorageProvider.saveCategoryList(onlineCategoryList);
+        localCategoryList = onlineCategoryList;
       }
 
-      List<Category> theNewestCategoryList = Category.getTheNewestCategoryList(
-          localCategoryList: localCategoryList,
-          onlineCategoryList: onlineCategoryList);
-
       emit(PersonalCategoryState.subscribedCategoryListLoaded(
-        subscribedCategoryList: theNewestCategoryList,
+        subscribedCategoryList: localCategoryList,
       ));
     } catch (e) {
       emit(PersonalCategoryState.subscribedCategoryListLoadingError(
@@ -55,8 +55,7 @@ class PersonalCategoryBloc
     Emitter<PersonalCategoryState> emit,
   ) {
     debugLog(event.toString());
-    _storage.setItem(
-        "categoryList", Category.categoryListToJson(event.categoryList));
+    localStorageProvider.saveCategoryList(event.categoryList);
     emit(PersonalCategoryState.subscribedCategoryListLoaded(
       subscribedCategoryList: event.categoryList,
     ));
