0a05869419409c554311404d55411640ae0e8cf0
Mike <mikelu@mirrormedia.mg>
2023-08-21 01:07:36 +0800

    Refactor:API K3->k6轉換

--- lib/pages/home/home_binding.dart
+++ lib/pages/home/home_binding.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:readr_app/data/providers/account_api_provider.dart';
 import 'package:readr_app/data/providers/articles_api_provider.dart';
+import 'package:readr_app/data/providers/local_storage_provider.dart';
 import 'package:readr_app/pages/home/home_controller.dart';
 
 import '../../services/auth_service.dart';
@@ -12,5 +13,6 @@ class HomeBinding extends Bindings {
     Get.put(ArticlesApiProvider.instance);
     Get.put(AccountApiProvider.instance);
     Get.put(AuthService.instance);
+    Get.put(LocalStorageProvider.instance);
   }
 }
