41b89b955e624208b78b899a5214f7b3b852a05b
Mike <mikelu@mirrormedia.mg>
2023-10-30 16:05:16 +0800

    Fix(Story Page): 修正影片顯示錯誤以及JWT token更新

--- lib/data/providers/auth_provider.dart
+++ lib/data/providers/auth_provider.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:readr_app/data/providers/auth_api_provider.dart';
@@ -13,20 +15,43 @@ class AuthProvider extends GetxController {
   final AuthApiProvider authApiProvider = Get.find();
   late String? idToken;
   final RxnString accessToken = RxnString();
+  User? userIndex;
 
   @override
   void onInit() {
     super.onInit();
     _auth.authStateChanges().listen(authStateChangesEvent);
+    Timer.periodic(const Duration(minutes: 55), updateJWTToken);
+  }
+
+  void updateJWTToken(Timer timer) {
+    if (userIndex != null) {
+      updateUserInfo(userIndex!);
+    }
   }
 
   void authStateChangesEvent(User? user) {
     if (user != null) {
       updateUserInfo(user);
+      userIndex = user;
     } else {
       idToken = null;
       accessToken.value = null;
     }
+  }
+
+  void authLogin() {
+    if (userIndex != null) {
+      updateUserInfo(userIndex!);
+    } else {
+      idToken = null;
+      accessToken.value = null;
+    }
+  }
+
+  void authLogout() {
+    idToken = null;
+    accessToken.value = null;
   }
 
   void updateUserInfo(User user) async {
