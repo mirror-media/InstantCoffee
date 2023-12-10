b16172417cb66e1bb0b9b79b617b6e4ba34f3f4f
Mike <mikelu@mirrormedia.mg>
2023-12-10 15:02:07 +0800

    Feat(Podcast page): Podcast Bug修正

--- lib/pages/home/premium/premium_home_widget.dart
+++ lib/pages/home/premium/premium_home_widget.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 import 'package:readr_app/blocs/section/cubit.dart';
 import 'package:readr_app/blocs/section/states.dart';
 import 'package:readr_app/blocs/tabContent/bloc.dart';
@@ -14,6 +15,9 @@ import 'package:readr_app/models/section.dart';
 import 'package:readr_app/pages/tabContent/listening/premium_listening_tab_content.dart';
 import 'package:readr_app/pages/tabContent/news/premium_tab_content.dart';
 import 'package:readr_app/pages/tabContent/personal/premium/premium_personal_tab_content.dart';
+import 'package:readr_app/pages/tabContent/podcast_tab_content/podcast_page.dart';
+import 'package:readr_app/pages/tabContent/podcast_tab_content/podcast_page_controller.dart';
+import 'package:readr_app/pages/tabContent/podcast_tab_content/widgets/podcast_sticky_panel/podcast_sticky_panel_controller.dart';
 import 'package:readr_app/services/record_service.dart';
 import 'package:readr_app/widgets/logger.dart';
 import 'package:readr_app/widgets/newsMarquee/news_marquee.dart';
@@ -52,6 +56,21 @@ class _PremiumHomeWidgetState extends State<PremiumHomeWidget>
     _tabs.clear();
     _tabWidgets.clear();
     _scrollControllerList.clear();
+    int indexToMove =
+        sectionItems.indexWhere((section) => section.title == 'Podcasts');
+
+    // 找到 '生活' 的 Section
+    int indexAfter =
+        sectionItems.indexWhere((section) => section.title == '生活');
+
+    // 如果找到了 'podcast' 和 '生活'，而且 'podcast' 在 '生活' 之前
+    if (indexToMove != -1 && indexAfter != -1 && indexToMove < indexAfter) {
+      // 取出 'podcast'
+      Section sectionToMove = sectionItems.removeAt(indexToMove);
+
+      // 插入到 '生活' 的後面
+      sectionItems.insert(indexAfter, sectionToMove);
+    }
 
     for (int i = 0; i < sectionItems.length; i++) {
       Section section = sectionItems[i];
@@ -84,6 +103,8 @@ class _PremiumHomeWidgetState extends State<PremiumHomeWidget>
         _tabWidgets.add(PremiumPersonalTabContent(
           scrollController: _scrollControllerList[i],
         ));
+      } else if (section.key == 'Podcasts') {
+        _tabWidgets.add(const PodcastPage());
       } else {
         _tabWidgets.add(BlocProvider(
             create: (context) => TabContentBloc(recordRepos: RecordService()),
@@ -105,9 +126,23 @@ class _PremiumHomeWidgetState extends State<PremiumHomeWidget>
           _tabController == null ? _initialTabIndex : _tabController!.index,
     )..addListener(() {
         _tabs.clear();
+
+        if (sectionItems[_tabController!.index].title == 'Podcasts') {
+          if (!Get.isRegistered<PodcastPageController>()) {
+            Get.put(PodcastPageController());
+          }
+          if (!Get.isRegistered<PodcastStickyPanelController>()) {
+            Get.put(PodcastStickyPanelController());
+          }
+        } else {
+          final PodcastPageController podcastPageController = Get.find();
+          podcastPageController.rxnSelectPodcastInfo.value = null;
+          podcastPageController.animationController.reverse();
+        }
+
         for (int i = 0; i < sectionItems.length; i++) {
           Section section = sectionItems[i];
-          String title = section.title?? StringDefault.valueNullDefault;
+          String title = section.title ?? StringDefault.valueNullDefault;
           Color? color;
           if (section.name == 'member') {
             title = 'Premium文章';
@@ -197,7 +232,8 @@ class _PremiumHomeWidgetState extends State<PremiumHomeWidget>
                     onTap: (int index) {
                       if (index >= 5) {
                         FirebaseAnalyticsHelper.logTabBarAfterTheSixthClick(
-                            sectiontitle: sections[index].title??StringDefault.valueNullDefault);
+                            sectiontitle: sections[index].title ??
+                                StringDefault.valueNullDefault);
                       }
 
                       if (_initialTabIndex == index) {
