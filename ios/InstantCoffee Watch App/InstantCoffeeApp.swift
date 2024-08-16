d7cfe5beb465cc12050d34530e8335506c715cd8
jnywu <johnnywu203@gmail.com>
2024-08-16 12:49:08 +0800

    新增 Watch target

---
+++ ios/InstantCoffee Watch App/InstantCoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantCoffeeApp.swift
+//  InstantCoffee Watch App
+//
+//  Created by 吳求元 on 2024/8/16.
+//
+
+import SwiftUI
+
+@main
+struct InstantCoffee_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/InstantCoffee Watch App/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InstantCoffee Watch App
+//
+//  Created by 吳求元 on 2024/8/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
