317e884f1d41199ca94ed07289ce6c17e791e68e
jnywu <johnnywu203@gmail.com>
2024-08-20 16:35:04 +0800

    Move mock data to individual file The data is only used in Xcode Preview

--- ios/InstantCoffee Watch App/View/HomePageView.swift
+++ ios/InstantCoffee Watch App/View/HomePageView.swift
@@ -66,9 +66,6 @@ struct HomePageView: View {
     }
 }
 
-var mockHeader = Header(order: 2, type: "category", slug: "news", name: "焦點", categories: [], sections: ["news"])
-var mockRSS = RSSItem(id: "2", title: "【巴黎奧運】開幕在即澳洲金牌選手裝備遭竊　車窗被砸破！小偷還吃光巧克力棒", description: "太誇張！巴黎奧運26日就要正式開幕，竟然有選手比賽裝備被偷！上屆奧運冠軍得主，澳洲的極限單車明星馬丁（Logan Martin）團隊的座車在比利時驚傳遭竊，不僅自行車包包被偷，歹徒甚至還吃光了他的巧克力棒只留下垃圾，所幸最重要的單車沒有失竊。", category: "國際要聞", pubDate: "Thu, 25 Jul 2024 16:34:00 +0800", imageUrl: "https://v3-statics.mirrormedia.mg/images/c85c87f1-ffd2-4c17-98c6-c141ceef1532-w1600.png")
-
 #Preview {
     
     TabView {
