9e15324d00a9b2b43a8a2b4fd20777789ef10e1e
jnywu <johnnywu203@gmail.com>
2024-08-23 14:16:33 +0800

    update mock data imageURL

--- ios/InstantCoffee Watch App/Extension/PreviewData.swift
+++ ios/InstantCoffee Watch App/Extension/PreviewData.swift
@@ -1,3 +1,3 @@
 // Mock Data for swift Preview only, not showing in actual UI
 var mockHeader = Header(order: 2, type: "testType", slug: "testSlug", name: "測試", categories: [], sections: ["news"])
-var mockRSS = RSSItem(id: "2", title: "測試", description: "測試內文測試內文測試內文測試內文，測試內文測試內文測試內文測試內文！測試內文測試內文測試內文測試內文，測試內文測試內文測試內文測試內文！", category: "測試分類", pubDate: "Thu, 23 Jan 2123 12:34:56 +0800", imageUrl: "https://v3-statics.mirrormedia.mg/images/c85c87f1-ffd2-4c17-98c6-c141ceef1532-w1600.png")
+var mockRSS = RSSItem(id: "2", title: "測試", description: "測試內文測試內文測試內文測試內文，測試內文測試內文測試內文測試內文！測試內文測試內文測試內文測試內文，測試內文測試內文測試內文測試內文！", category: "測試分類", pubDate: "Thu, 23 Jan 2123 12:34:56 +0800", imageUrl: "https://doodleipsum.com/1600/abstract?bg=D96363&i=be149914c1ca3abd4d013b3b7bb43666")
