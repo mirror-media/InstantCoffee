e4b5825666d9d0cfec80dc50959b1623e74ff3db
jnywu <johnnywu203@gmail.com>
2024-09-23 10:51:57 +0800

    1.0 build 2, add loading screen

--- ios/InstantCoffee Watch App/View/MainTabView.swift
+++ ios/InstantCoffee Watch App/View/MainTabView.swift
@@ -6,10 +6,12 @@ struct MainTabView: View {
     @State private var stories: [RSSItem] = []
     @State private var storiesByCategory: [String: [RSSItem]] = [:]
     @State private var categoryToSectionMap: [String: String] = [:]
-    
+    @State private var isLoading: Bool = false
+    @State private var hasAppeared: Bool = false
+
     let storyURL = URL(string: Bundle.main.infoDictionary!["storyURL"] as! String)
-    
-    func getRSSItems() {
+
+    func getRSSItems() async {
         guard let url = storyURL else { return }
         let parser = RSSParser()
         parser.parse(from: url) { items in
@@ -17,25 +19,34 @@ struct MainTabView: View {
             self.storiesByCategory = organizeStoriesByCategory(stories: items, headers: self.headers, categoryToSectionMap: categoryToSectionMap)
         }
     }
-    
+
     var body: some View {
         NavigationStack {
-            TabView {
-                ForEach(headers) { header in
-                    if let stories = storiesByCategory[header.name] {
-                        HomePageView(header: header, stories: stories)
+            if isLoading {
+                ProgressView("正在載入")
+            } else {
+                TabView {
+                    ForEach(headers) { header in
+                        if let stories = storiesByCategory[header.name] {
+                            HomePageView(header: header, stories: stories)
+                        }
                     }
                 }
+                .navigationTitle("鏡週刊")
             }
-            .task {
-                do {
-                    let (fetchedHeaders, map) = try await fetchHeaders()
-                    headers = Array(fetchedHeaders.dropFirst())
-                    categoryToSectionMap = map
-                    getRSSItems()
-                } catch {}
-            }
-            .navigationTitle("鏡週刊")
+        }
+        .task {
+            print("isLoading")
+            isLoading = true
+            do {
+                let (fetchedHeaders, map) = try await fetchHeaders()
+                headers = Array(fetchedHeaders.dropFirst())
+                categoryToSectionMap = map
+                await getRSSItems()
+                print("getRSSItems end")
+            } catch {}
+            isLoading = false
+            print("isLoading end")
         }
     }
 }
