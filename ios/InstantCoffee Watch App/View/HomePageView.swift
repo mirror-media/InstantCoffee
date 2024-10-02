6143d909c97c8ff1b1de58045e30d59ad0105312
jnywu <johnnywu203@gmail.com>
2024-10-02 16:22:02 +0800

    change AsyncImage to CachedImage package

--- ios/InstantCoffee Watch App/View/HomePageView.swift
+++ ios/InstantCoffee Watch App/View/HomePageView.swift
@@ -1,70 +1,62 @@
 
 import SwiftUI
+import CachedImage
 
 struct HomePageView: View {
-    
     let header: Header
     let stories: [RSSItem]
     @State private var backgroundImage: Image? = nil
-    @State private var retry: Bool = false
 
     var body: some View {
-        
-        VStack(alignment: .leading, spacing: 5) {
-            Spacer()
-            Text(header.name)
-                .font(.headline)
-                .padding(.vertical, 2)
-                .padding(.horizontal, 4)
-                .background(header.color)
-                .clipShape(RoundedRectangle(cornerRadius: 6))
-            Text(stories.first!.title ?? "")
-                .font(.headline)
-                .fixedSize(horizontal: false, vertical: true)
-                .lineLimit(2)
-            Text(stories.first!.date ?? "")
-                .foregroundStyle(header.color)
-            
-            NavigationLink {
-                ListView(header: header, stories: stories)
-                    .navigationTitle(header.name)
-            } label: {
-                Text("更多" + header.name)
-            }
-            .controlSize(.small)
-            .buttonStyle(BorderedProminentButtonStyle())
-            .buttonBorderShape(.roundedRectangle(radius: 13.0))
-            .tint(header.color)
-        }
-        .padding(4)
-        .background(
-            AsyncImage(url: URL(string: stories.first?.imageUrl ?? "")) { phase in
-                switch phase {
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(minWidth: 0, maxWidth: .infinity)
-                            .clipped()
-                            .overlay() {
-                                LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
-                            }
-                            .ignoresSafeArea()
-                            .opacity(0.4)
-                    case .failure(_):
-                        VStack {
-                            Image(systemName: "exclamationmark.triangle.fill")
-//                            Button("Retry") {
-//                                retry.toggle()
-//                            }
+        ZStack {
+            // background image
+            CachedImage(
+                url: URL(string: stories.first?.imageUrl ?? ""),
+                content: { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .clipped()
+                        .overlay() {
+                            LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
                         }
-                    case .empty:
-                        ProgressView()
-                    default:
-                        ProgressView()
+                        .ignoresSafeArea()
+                        .opacity(0.4)
+                }, placeholder: {
+                    ProgressView()
                 }
+            )
+
+            // foreground
+            VStack(alignment: .leading, spacing: 5) {
+                Spacer()
+                Text(header.name)
+                    .font(.headline)
+                    .padding(.vertical, 2)
+                    .padding(.horizontal, 4)
+                    .background(header.color)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+                Text(stories.first!.title ?? "")
+                    .font(.headline)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .lineLimit(2)
+                Text(stories.first!.date ?? "")
+                    .foregroundStyle(header.color)
+                
+                NavigationLink {
+                    ListView(header: header, stories: stories)
+                        .navigationTitle(header.name)
+                } label: {
+                    Text("更多" + header.name)
+                }
+                .controlSize(.small)
+                .buttonStyle(BorderedProminentButtonStyle())
+                .buttonBorderShape(.roundedRectangle(radius: 13.0))
+                .tint(header.color)
             }
-        )
+            .padding(4)
+        }
     }
 }
 
